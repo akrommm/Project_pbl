58065443f4d55e758d4b00429752c354795305f5
akrommm <102010542+akrommm@users.noreply.github.com>
2022-10-19 14:24:14 +0700

    Update index.blade.php

--- resources/views/admin/master-data/pegawai/index.blade.php
+++ resources/views/admin/master-data/pegawai/index.blade.php
@@ -6,7 +6,6 @@
     <div class="card mt-4">
         <div class="card-header">
             <div class="card-title">
-                Data Pegawai
             </div>
             <a href="{{ url('admin/master-data/pegawai/create') }}" class="float-right btn btn-dark"><i class="fas fa-plus"></i> Tambah Data</a>
         </div>
